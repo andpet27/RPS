43a5115578ed31d2bcc46f21e92c81c623e41bcf
Andrej <peternelandrej0@gmail.com>
2020-03-14 01:50:15 +0100

    update

--- GUI/RpsGui.py
+++ GUI/RpsGui.py
@@ -3,13 +3,22 @@ from tkinter import *
 start = Tk()
 start.title("Rock, Paper, Scissors")
 
-canvas = Canvas(start, height=750, width=1250, bg="#4a4e54")
+canvasStart = Canvas(start, height=750, width=1250, bg="#4a4e54")
 start.resizable(False, False)
-canvas.create_text(650, 80, fill="white", text="Rock,\nPaper,\nScissors", anchor=N, font=("Helvetica", 100))
+canvasStart.create_text(650, 80, fill="white", text="Rock,\nPaper,\nScissors", anchor=N, font=("Helvetica", 100))
 
-btnStart = Button(text="Play", font=("Helvetica", 30))
+def callCanvasGame():
+    game = Tk()
+    game.title("Rock, Paper, Scissors")
+    canvasGame = Canvas(game, height=750, width=1250, bg="#4a4e54")
+    game.resizable(False,False)
+    
+    canvasGame.pack()
+    start.destroy()
+
+btnStart = Button(text="Play", font=("Helvetica", 30), command=callCanvasGame)
 btnStart.configure(width=16, height=1, activebackground="#4a4e54")
-btnStart_window = canvas.create_window(610,620, window=btnStart)
+btnStart_window = canvasStart.create_window(610,620, window=btnStart)
 
-canvas.pack()
+canvasStart.pack()
 start.mainloop()
