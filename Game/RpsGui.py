10e3db57b0439a4932c7e0624183bef6afe4f4b4
andpet27 <62115025+andpet27@users.noreply.github.com>
2020-03-14 20:02:08 +0100

    buttons

--- Game/RpsGui.py
+++ Game/RpsGui.py
@@ -4,26 +4,43 @@ from game import *
 start = Tk()
 start.title("Rock, Paper, Scissors")
 
+play = True
+
 canvasStart = Canvas(start, height=750, width=1250, bg="#4a4e54")
 start.resizable(False, False)
 canvasStart.create_text(650, 80, fill="white", text="Rock,\nPaper,\nScissors", anchor=N, font=("Helvetica", 100))
 
+
 def callCanvasGame():
+    start.destroy()
     game = Tk()
     game.title("Rock, Paper, Scissors")
     canvasGame = Canvas(game, height=750, width=1250, bg="#4a4e54")
     game.resizable(False,False)
+
+    score = "Score: Player %d - %d Computer" %(playerSc,computerSc)
+    canvasGame.create_text(600, 100, fill="white", text=score, anchor=S, font=("Helvetica", 40))
+    
     
 
-    canvasGame.create_text(600, 100, fill="white", text=("Score: Player %d - %d Computer" %(playerSc,computerSc)), anchor=S, font=("Helvetica", 40))
+    btnRock = Button(text="Rock", font=("Helvetica", 10))
+    btnRock.configure(width=10, height=1, activebackground="#a3a29d")
+    btnRock_window = canvasGame.create_window(400, 700, window=btnRock)
+    
 
+    btnPaper = Button(text="Paper", font=("Helvetica", 10))
+    btnPaper.configure(width=10, height=1, activebackground="#a3a29d")
+    btnPaper_window = canvasGame.create_window(600, 700, window=btnPaper)
+
+    btnScissors = Button(text="Scissors", font=("Helvetica", 10))
+    btnScissors.configure(width=10, height=1, activebackground="#a3a29d")
+    btnScissors_window = canvasGame.create_window(800, 700, window=btnScissors)
 
     canvasGame.pack()
-    start.destroy()
 
 btnStart = Button(text="Play", font=("Helvetica", 30), command=callCanvasGame)
 btnStart.configure(width=16, height=1, activebackground="#a3a29d")
-btnStart_window = canvasStart.create_window(610,620, window=btnStart)
+btnStart_window = canvasStart.create_window(610, 620, window=btnStart)
 
 canvasStart.pack()
 start.mainloop()
--- Game/game.py
+++ Game/game.py
@@ -1,67 +1,42 @@
 from random import randint
 
 playerCh = int()
-play = "yes"
+computerCh = randint(1,3)
 computerSc = 0
 playerSc = 0        
 
-while play  == "yes":
-        
-    playerCh = int(input("Choose:\n1) Rock \n2) Paper \n3) Scissors \n"))
-    while playerCh > 3:
-        playerCh = int(input("Choose:\n1) Rock \n2) Paper \n3) Scissors \n"))
-
-    computerCh = randint(1, 3)
-
-
-    def win():
-        print("You win.")
-        global playerSc 
-        playerSc += 1
-        return playerSc
+def win():
+    print("You win.")
+    global playerSc 
+    playerSc += 1
+    return playerSc
     
-    def lose():
-        print("You lose.")
-        global computerSc
-        computerSc += 1
-        return computerSc
+def lose():
+    print("You lose.")
+    global computerSc
+    computerSc += 1
+    return computerSc
 
-    def rock(a):
-        if a == 1:
-            print("Draw.")
-        if a == 2:
-            lose()
-        if a == 3:
-            win()
+def rock(a):
+    if a == 1:
+        print("Draw.")
+    if a == 2:
+        lose()
+    if a == 3:
+        win()
     
-    def paper(a):
-        if a == 1:
-            win()
-        if a == 2:
-            print("Draw.")
-        if a == 3:
-            lose()
+def paper(a):
+    if a == 1:
+        win()
+    if a == 2:
+        print("Draw.")
+    if a == 3:
+        lose()
     
-    def scissors(a):
-        if a == 1:
-            lose()
-        if a == 2:
-            win()
-        if a == 3:
-            print("Draw.")
-
-    if computerCh == 1:
-        rock(playerCh)
-    if computerCh == 2:
-        paper(playerCh)
-    if computerCh == 3:
-        scissors(playerCh)       
-
-   
-
-    print("You chose: %d \nComputer chose: %d" %(playerCh, computerCh))
-    print("Score: You: %d Computer: %d" %(playerSc, computerSc))
-    
-    play = input("Play again (yes/no)?\n")
-
-print("Final score: You: %d Computer: %d. Goodbye" %(playerSc, computerSc))
+def scissors(a):
+    if a == 1:
+        lose()
+    if a == 2:
+        win()
+    if a == 3:
+        print("Draw.")
