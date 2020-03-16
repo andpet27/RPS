1c0102f5782e3f9621f1ce5d1b0244f7d6f0061b
andpet27 <62115025+andpet27@users.noreply.github.com>
2020-03-16 13:04:39 +0100

    few minor updates

--- Game/RpsGui.py
+++ Game/RpsGui.py
@@ -15,7 +15,7 @@ def callCanvasGame():
     game.title("Rock, Paper, Scissors")
     canvasGame = Canvas(game, height=750, width=1250, bg="#4a4e54")
     game.resizable(False,False)
-    
+
     btnRock = Button(text="Rock", font=("Helvetica", 10), command=lambda:gameLogic(1))
     btnRock.configure(width=10, height=1, activebackground="#a3a29d")
     btnRock_window = canvasGame.create_window(400, 700, window=btnRock)    
@@ -28,8 +28,10 @@ def callCanvasGame():
     btnScissors.configure(width=10, height=1, activebackground="#a3a29d")
     btnScissors_window = canvasGame.create_window(800, 700, window=btnScissors)
 
-    score = "Score: Player %d - %d Computer" %(playerSc,computerSc)
-    canvasGame.create_text(600, 100, fill="white", text=score, anchor=S, font=("Helvetica", 40))
+    def score(a,b):
+        print("Score: Player %d - %d Computer" %(a,b))
+
+    canvasGame.create_text(600, 100, fill="white", text=score(playerSc, computerSc), anchor=S, font=("Helvetica", 40))
 
     canvasGame.pack()
     game.mainloop()
